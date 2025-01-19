565f06bb8e80404e5b1be6cf53dee4461c96f8f7
murilo <murilocarrijoadm@gmail.com>
2025-01-19 18:45:02 -0300

    add `database.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-// "/api/status"
+import database from "../../../../infra/database.js";
 
-const status = (_req, res) => {
+const status = async (req, res) => {
+  const resutl = await database.query("SELECT 1 + 1 as sum;");
+  console.log("resutl =>", resutl.rows);
   return res.status(200).json({ status: "Ok" });
 };
 
